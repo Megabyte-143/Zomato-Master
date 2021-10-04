938fad262bea7fcad3cdc4d7a087424a2d57fdd4
Yash Garg <yashygkf@gmail.com>
2021-10-04 13:53:15 +0530

    completed API and AWS configuartion

--- server/index.js
+++ server/index.js
@@ -3,14 +3,19 @@ require("dotenv").config();
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
-import passport from "passport";
+// import passport from "passport";
 
 // Google Auth Config File
-import googleAuthConfig from "./config/google_auth";
+// import googleAuthConfig from "./config/google_auth";
 
 // API 
 import Auth from "./api/auth/index";
-import Restraunt from "./api/restraunts/index";
+import Restraunt from "./api/restraunt/index";
+import Food from "./api/food/index";
+import Menu from "./api/menu/index";
+import Image from "./api/image/index";
+import Order from "./api/orders/index";
+import Review from "./api/review/index";
 
 //Database Connection
 import ConnectDB from "./schema/connection";
@@ -22,17 +27,22 @@ zomato.use(express.json());
 zomato.use(express.urlencoded({ extended: false }));
 zomato.use(helmet());
 zomato.use(cors());
-zomato.use(passport.initialize());
-zomato.use(passport.session());
+// zomato.use(passport.initialize());
+// zomato.use(passport.session());
 
 // Passport Configuration 
-googleAuthConfig(passport);
+// googleAuthConfig(passport);
 
 
 //For application routes
 // localhost:4000/auth/signup
 zomato.use("/auth", Auth);
 zomato.use("/restraunt", Restraunt);
+zomato.use("/food", Food);
+zomato.use("/menu",Menu);
+zomato.use("/image",Image);
+zomato.use("/reviews",Review);
+zomato.use("/order",Order);
 
 zomato.get("/", (req, res) => res.json({ message: "SetUp Success Yay!!" }));
 
