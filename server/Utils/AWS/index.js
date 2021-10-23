10935887f79158dd0eb4248566c3e811ef87c7a8
Yash Garg <yashygkf@gmail.com>
2021-10-23 10:46:49 +0530

    aws hidden

--- server/Utils/AWS/index.js
+++ server/Utils/AWS/index.js
@@ -1,10 +1,11 @@
-import AWS from "aws-sdk";
+require("dotenv").config();
 
+import AWS from "aws-sdk";
 
 //AWS S3 config
 const s3Buket = new AWS.S3({
-    accessKeyId: "AKIA6FFOC2W6R5XWR372",
-    secretAccessKey: "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",
+    accessKeyId: process.env.AWS_S3_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_S3_SECRET_KEY,
     region: "ap-south-1"
 })
 
